f44664f59b92b5580c63bb67d978526dce97a2ea
Robin Phillips <rlphillips@deloitte.co.uk>
2022-12-01 07:00:53 +0000

    day 1 complete

--- Sources/AdventOfCode/Day 1/Day1.swift
+++ Sources/AdventOfCode/Day 1/Day1.swift
@@ -1,24 +1,50 @@
 struct Day1: Solution {
     static let day = 1
     
+    var calories: [Int] = []
+    
     /// Initialise your solution
     ///
     /// - parameters:
     ///   - input: Contents of the `Day1.input` file within the same folder as this source file
     init(input: String) {
-
+        calories = input
+            .components(separatedBy: "\n\n")
+            .compactMap {
+                $0.components(separatedBy: .newlines)
+                    .compactMap(Int.init)
+                    
+            }
+            .compactMap { $0.reduce(0, +) }
+            .sorted()
+        
+        print(calories)
     }
 
     /// Return your answer to the main activity of the advent calendar
     ///
     /// If you need to, you can change the return type of this method to any type that conforms to `CustomStringConvertible`, i.e. `String`, `Float`, etc.
     func calculatePartOne() -> Int {
-        0
+        calculateHighestCalories()
     }
 
     /// Return your solution to the extension activity
     /// _ N.B. This is only unlocked when you have completed part one! _
     func calculatePartTwo() -> Int {
-        0
+        calculateHighest3()
+    }
+}
+
+extension Day1 {
+    func calculateHighestCalories() -> Int {
+        calories.last ?? 0
+    }
+    
+    func calculateHighest3() -> Int {
+        let count = calories.count
+        
+        return calories[calories.count-1]
+        + calories[calories.count-2]
+        + calories[calories.count-3]
     }
 }
