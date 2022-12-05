d8f16831d0e5a36bad732f6e0f7e08008410b5e1
Robin Phillips <rlphillips@deloitte.co.uk>
2022-12-05 20:08:18 +0000

    day 5 complete

--- Sources/AdventOfCode/Day 5/Day5.swift
+++ Sources/AdventOfCode/Day 5/Day5.swift
@@ -2,6 +2,7 @@ struct Day5: Solution {
     static let day = 5
     
     let moves: [[Int]]
+    var startSupplies: [Stack] = []
     
     init(input: String) {
         moves = input
@@ -11,73 +12,62 @@ struct Day5: Solution {
                 $0.components(separatedBy: " ")
                     .compactMap(Int.init)
             }
-        print(moves)
+
+        startSupplies = [
+            Stack(id: 1, crates: ["B", "S", "V", "Z", "G", "P", "W"]),
+            Stack(id: 2, crates: ["J", "V", "B", "C", "Z", "F"]),
+            Stack(id: 3, crates: ["V", "L", "M", "H", "N", "Z", "D", "C"]),
+            Stack(id: 4, crates: ["L", "D", "M", "Z", "P", "F", "J", "B"]),
+            Stack(id: 5, crates: ["V", "F", "C", "G", "J", "B", "Q", "H"]),
+            Stack(id: 6, crates: ["G", "F", "Q", "T", "S", "L", "B"]),
+            Stack(id: 7, crates: ["L", "G", "C", "Z", "V"]),
+            Stack(id: 8, crates: ["N", "L", "G"]),
+            Stack(id: 9, crates: ["J", "F", "H", "C"])
+        ]
     }
     
-    func calculatePartOne() -> Int {
-//        print( part1() )
-        return 0
+    func calculatePartOne() -> String {
+        part1(supplies: startSupplies)
     }
     
-    func calculatePartTwo() -> Int {
-        print( part2() )
-        return 0
+    func calculatePartTwo() -> String {
+        part2(supplies: startSupplies)
     }
 }
 
 extension Day5 {
-    func part1() -> String {
+    func part1(supplies: [Stack]) -> String {
+        var tempSupplies = supplies
+        
         moves.forEach {
-            Supplies.moveCratesPart1(num: $0[0], from: $0[1], to: $0[2])
+            for _ in 0 ..< $0[0] {
+                let crate = tempSupplies[$0[1]-1].crates.removeLast()
+                            tempSupplies[$0[2]-1].crates.append(crate)
+            }
         }
         
-        let topOfStack: [String] = Supplies.supplies.compactMap {
+        let topOfStack: [String] = tempSupplies.compactMap {
             $0.topOfstack
-            
         }
         return topOfStack.joined()
     }
     
-    func part2() -> String {
+    func part2(supplies: [Stack]) -> String {
+        var tempSupplies = supplies
+        
         moves.forEach {
-            Supplies.moveCratesPart2(num: $0[0], from: $0[1], to: $0[2])
+            var crates: [String] = []
+            
+            for _ in 0 ..< $0[0] {
+                crates.insert(tempSupplies[$0[1]-1].crates.removeLast(), at: 0)
+            }
+            tempSupplies[$0[2]-1].crates.append(contentsOf: crates)
         }
         
-        let topOfStack: [String] = Supplies.supplies.compactMap {
+        let topOfStack: [String] = tempSupplies.compactMap {
             $0.topOfstack
-            
         }
         return topOfStack.joined()
     }
 }
 
-
-struct Supplies {
-    static var supplies: [Stack]  = [
-        Stack(id: 1, crates: ["B", "S", "V", "Z", "G", "P", "W"]),
-        Stack(id: 2, crates: ["J", "V", "B", "C", "Z", "F"]),
-        Stack(id: 3, crates: ["V", "L", "M", "H", "N", "Z", "D", "C"]),
-        Stack(id: 4, crates: ["L", "D", "M", "Z", "P", "F", "J", "B"]),
-        Stack(id: 5, crates: ["V", "F", "C", "G", "J", "B", "Q", "H"]),
-        Stack(id: 6, crates: ["G", "F", "Q", "T", "S", "L", "B"]),
-        Stack(id: 7, crates: ["L", "G", "C", "Z", "V"]),
-        Stack(id: 8, crates: ["N", "L", "G"]),
-        Stack(id: 9, crates: ["J", "F", "H", "C"])
-    ]
-    
-    static func moveCratesPart1(num: Int, from: Int, to: Int) {
-        for _ in 0 ..< num {
-            let crate = supplies[from-1].crates.removeLast()
-            supplies[to-1].crates.append(crate)
-        }
-    }
-    
-    static func moveCratesPart2(num: Int, from: Int, to: Int) {
-        var crates: [String] = []
-        for i in 0 ..< num {
-            crates.insert(supplies[from-1].crates.removeLast(), at: 0)
-        }
-        
-        supplies[to-1].crates.append(contentsOf: crates)
-    }
-}
--- Tests/AdventOfCodeTests/Day 5/Day5Tests.swift
+++ Tests/AdventOfCodeTests/Day 5/Day5Tests.swift
@@ -1,7 +1,7 @@
 import XCTest
 @testable import AdventOfCode
 
-final class Day5Tests: XCTestCase {
+final class Day5Tests: XCTestCase, SolutionTest {
     typealias SUT = Day5
     
     var testSupplies: [Stack] = [
@@ -10,22 +10,22 @@ final class Day5Tests: XCTestCase {
         Stack(id: 3, crates: ["P"])
     ]
     
-//    func testPartOne() throws {
-//        try XCTAssertEqual(sut.calculatePartOne(), "CMZ")
-//    }
-//
-//    func testPartTwo() throws {
-//        try XCTAssertEqual(sut.calculatePartTwo(), 0)
-//    }
-//}
-//
-//extension Day5Tests {
-//    func test_part1_moves() throws {
-//        try XCTAssertEqual(sut.moves[0], [1,2,1])
-//        try XCTAssertEqual(sut.moves[1], [3,1,3])
-//        try XCTAssertEqual(sut.moves[2], [2,2,1])
-//        try XCTAssertEqual(sut.moves[3], [1,1,2])
-//    }
+    func testPartOne() throws {
+        try XCTAssertEqual(sut.part1(supplies: testSupplies), "CMZ")
+    }
+
+    func testPartTwo() throws {
+        try XCTAssertEqual(sut.part2(supplies: testSupplies), "MCD")
+    }
+}
+
+extension Day5Tests {
+    func test_part1_moves() throws {
+        try XCTAssertEqual(sut.moves[0], [1,2,1])
+        try XCTAssertEqual(sut.moves[1], [3,1,3])
+        try XCTAssertEqual(sut.moves[2], [2,2,1])
+        try XCTAssertEqual(sut.moves[3], [1,1,2])
+    }
     
     func test_part1_startingCrates() throws {
         XCTAssertEqual(testSupplies[0].crates, ["Z", "N"])
