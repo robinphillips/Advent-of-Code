d06ec6bd7bb66414779c5e8f2deb609852147722
Robin Phillips <rlphillips@deloitte.co.uk>
2022-12-06 06:40:44 +0000

    day 6 complete

--- Tests/AdventOfCodeTests/Day 6/Day6Tests.swift
+++ Tests/AdventOfCodeTests/Day 6/Day6Tests.swift
@@ -5,20 +5,10 @@ final class Day6Tests: XCTestCase, SolutionTest {
     typealias SUT = Day6
     
     func testPartOne() throws {
-        try XCTAssertEqual(sut.calculatePartOne(), 0)
+        try XCTAssertEqual(sut.calculatePartOne(), 5)
     }
     
     func testPartTwo() throws {
-        try XCTAssertEqual(sut.calculatePartTwo(), 0)
-    }
-}
-
-extension Day6Tests {
-    func test_part1() throws {
-        try XCTAssertEqual(sut.processSignalSearch(lengthOfStart: 4), 5)
-    }
-    
-    func test_part2() throws {
-        try XCTAssertEqual(sut.processSignalSearch(lengthOfStart: 14), 23)
+        try XCTAssertEqual(sut.calculatePartTwo(), 23)
     }
 }
