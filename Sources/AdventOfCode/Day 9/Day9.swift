2cee7a3524a7da82859a0b193d7dc2e7b5221122
Robin Phillips <rlphillips@deloitte.co.uk>
2022-12-09 10:59:12 +0000

    Day 9 wip

--- Sources/AdventOfCode/Day 9/Day9.swift
+++ Sources/AdventOfCode/Day 9/Day9.swift
@@ -1,8 +1,18 @@
 struct Day9: Solution {
     static let day = 9
     
+//    let instructions: [Move]
+    let strings: [String]
+    
     init(input: String) {
+        strings = input
+            .components(separatedBy: .newlines)
+            .filter { $0.isNotEmpty}
         
+        instructions = strings
+            .compactMap(<#T##transform: (String) throws -> ElementOfResult?##(String) throws -> ElementOfResult?#>)
+        
+        print(strings)
     }
     
     func calculatePartOne() -> Int {
@@ -13,3 +23,85 @@ struct Day9: Solution {
         0
     }
 }
+
+extension Day9 {
+    func processMove(instruction: String) -> Move {
+        
+        let parts = instruction
+            .components(separatedBy: .whitespaces)
+        
+        
+        let direction = parts.first
+        let distance = parts.last
+        
+        var move = Move(rawValue: direction)
+        
+        switch move {
+        case .up:
+            print("up")
+        case .right:
+            print("right")
+        case .down:
+            print("down")
+        case .left:
+            print("left")
+            
+        }
+    }
+    
+    func tailPosition(newHeadPos: BridgePoint, prevHeadPos: BridgePoint, tailPos: BridgePoint) -> BridgePoint {
+        // if currentHeadPos is more than 1 away from tailPos
+        
+        guard let distance = checkDistanceBetweenPoints(point1: newHeadPos, point2: tailPos) else { return prevHeadPos }
+        print("distance: ", distance)
+        if distance > 1 { // function to check for distance
+            return prevHeadPos // then return prev head pos
+        } else {
+            return tailPos
+        }
+    }
+    
+    func checkDistanceBetweenPoints(point1: BridgePoint, point2: BridgePoint) -> Int? {
+        if abs(point1.x - point2.x) > 1 || abs(point1.y - point2.y) > 1 {
+            return 2
+        } else if abs(point1.x - point2.x) == 1 && abs(point1.y - point2.y) == 1 {
+            return 1
+        } else if abs(point1.x - point2.x) == 1 || abs(point1.y - point2.y) == 1 {
+            return 1
+        } else if abs(point1.x - point2.x) > 2 || abs(point1.y - point2.y) > 2 {
+            print("soemthing went wrong, distance is too big")
+            return nil
+        } else {
+            return 0
+        }
+    }
+}
+
+struct BridgePoint: Hashable {
+    let x: Int
+    let y: Int
+}
+
+//enum Move: Int {
+//    case up = distance
+//    case right = distance
+//    case down = distance
+//    case left = distance
+//
+//    init?(rawValue: String, distance: Int) {
+//        switch rawValue {
+//        case "U":
+//            self = .up
+//        case "R":
+//            self = .right
+//        case "D":
+//            self = .down
+//        case "L":
+//            self = .left
+//        default:
+//            preconditionFailure()
+//        }
+//    }
+//}
+
+
--- Tests/AdventOfCodeTests/Day 9/Day9Tests.swift
+++ Tests/AdventOfCodeTests/Day 9/Day9Tests.swift
@@ -12,3 +12,30 @@ final class Day9Tests: XCTestCase, SolutionTest {
         try XCTAssertEqual(sut.calculatePartTwo(), 0)
     }
 }
+
+
+extension Day9Tests {
+    func test_part1_distanceBetweenPoints() {
+        try XCTAssertEqual(sut.checkDistanceBetweenPoints(point1: BridgePoint(x: 0, y: 0), point2: BridgePoint(x: 1, y: 2)), 2)
+        
+        try XCTAssertEqual(sut.checkDistanceBetweenPoints(point1: BridgePoint(x: 3, y: 1), point2: BridgePoint(x: 1, y: 1)), 2)
+    }
+    
+    func test_part1_tailposition() {
+        
+        try XCTAssertEqual(sut.tailPosition(newHeadPos: BridgePoint(x: 3, y: 1),
+                                            prevHeadPos: BridgePoint(x: 2, y: 1),
+                                            tailPos: BridgePoint(x: 1, y: 1)),
+                           BridgePoint(x: 2, y: 1))
+        
+        try XCTAssertEqual(sut.tailPosition(newHeadPos: BridgePoint(x: 1, y: 3),
+                                            prevHeadPos: BridgePoint(x: 2, y: 1),
+                                            tailPos: BridgePoint(x: 1, y: 1)),
+                           BridgePoint(x: 2, y: 1))
+        
+        try XCTAssertEqual(sut.tailPosition(newHeadPos: BridgePoint(x: 4, y: 2),
+                                            prevHeadPos: BridgePoint(x: 4, y: 1),
+                                            tailPos: BridgePoint(x: 3, y: 0)),
+                           BridgePoint(x: 4, y: 1))
+    }
+}
